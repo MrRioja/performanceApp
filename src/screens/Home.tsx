94935e29e22945327f6d653a68184b83cc45b5fa
Luiz Rioja <lulyrioja@gmail.com>
2021-11-02 23:44:11 -0300

    :alien: Implementing the home screen

---
+++ src/screens/Home.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+import { View, Text, TextInput, StyleSheet, Button } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { FriendList } from "../components/FriendList";
+
+export function Home() {
+  const [name, setName] = useState("");
+  const [friends, setFriends] = useState([]);
+
+  async function handleSearch() {
+    const response = await fetch(`http://10.0.0.132:3333/friends?q=${name}`);
+    const data = await response.json();
+    setFriends(data);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Amigos</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do cliente"
+        onChangeText={setName}
+      />
+
+      <Button title="Buscar" onPress={handleSearch} />
+
+      <ScrollView style={styles.list}>
+        <FriendList data={friends} />
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 100,
+    padding: 25,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  input: {
+    borderWidth: 1,
+    padding: 7,
+    marginVertical: 10,
+  },
+  list: {
+    marginTop: 20,
+  },
+});
